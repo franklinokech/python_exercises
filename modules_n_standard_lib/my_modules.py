af3e54ef5058be533da713683714a2fc57423796
Franklin okech <franklinokech@gmail.com>
2020-02-04 03:17:50 +0300

    added the modules functionality

---
+++ modules_n_standard_lib/my_modules.py
@@ -0,0 +1,16 @@
+# this module will be used by other modules
+print('This is an imported module')
+test_string = 'Test String'
+
+
+def find_index(to_search, target):
+    """Finds the index of a value in the sequence
+
+    Args:
+        to_search (list): list to search from
+        target (list): what we are looking for
+    """
+    for i, value in enumerate(to_search):
+        if value == target:
+            return i
+        return -1
